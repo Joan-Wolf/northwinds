450a65a41131e5698df0f17f5baaaf1254fe8f3c
Joan Wolf <joanewolf@gmail.com>
2023-04-10 18:26:46 -0400

    final commit

--- northwinds.sql
+++ northwinds.sql
@@ -36,27 +36,54 @@
 -- UPDATE orders SET ship_region = 'EuroZone' WHERE ship_country = 'France';
 
 -- 10. Delete all rows from order_details that have a quantity of 1. 
-
+-- DELETE FROM order_details WHERE quantity = 1;
 
 -- 11. Calculate the average, max, and min of the quantity in the order_details table.
-
+-- SELECT AVG(quantity)FROM order_details;
+-- SELECT MAX(quantity)FROM order_details;
+-- SELECT MIN(quantity)FROM order_details;
 
 -- 12. Calculate the average, max, and min of the quantity in the order_details table, grouped by the order_id.
-
+-- SELECT AVG(quantity) FROM order_details GROUP BY order_id;
+-- SELECT MAX(quantity) FROM order_details GROUP BY order_id;
+-- SELECT MIN(quantity) FROM order_details GROUP BY order_id;
 
 -- 13. Find the customer_id that placed order 10290 (orders table)
-
+-- SELECT customer_id FROM orders
+-- WHERE order_id = '10290';
 
 -- 14. Do an inner join, left join, right join on orders and customers tables.  (These are three separate queries, one for each type of join.)
+-- SELECT customers.customer_id, order_id, order_date, shipped_date
+-- FROM customers
+-- INNER JOIN orders on customers.customer_id = orders.customer_id;
 
+-- SELECT customers.customer_id, order_id, order_date, shipped_date
+-- FROM customers
+-- LEFT JOIN orders on customers.customer_id = orders.customer_id;
+
+-- SELECT customers.customer_id, order_id, order_date, shipped_date
+-- FROM customers
+-- RIGHT JOIN orders on customers.customer_id = orders.customer_id;
 
 -- 15. Use a join to get the ship_city and ship_country of all the orders which are associated with an employee who is in London.
-
+-- SELECT orders.employee_id, ship_city, ship_country, employees.city
+-- FROM orders
+-- RIGHT JOIN employees on orders.employee_id = employees.employee_id
+-- WHERE employees.city = 'London';
 
 -- 16. Use a join to get the ship_name of all orders that include a discontinued product. (See orders, order_details, and products. 1 means discontinued.)
-
+--SELECT orders.ship_name, order_details.order_id,products.discontinued
+--FROM orders 
+--INNER JOIN order_details 
+--ON orders.order_id = order_details.order_id
+--INNER JOIN products 
+--ON products.product_id = order_details.product_id
+--WHERE products.discontinued ='1';
 
 -- 17. Get first names of all employees who report to no one.
-
+--SELECT first_name, reports_to FROM employees
+--WHERE reports_to IS NULL;
 
 -- 18. Get first names of all employees who report to 'Andrew'.
+--SELECT first_name, reports_to, FROM employees
+--WHERE reports_to = '2';
